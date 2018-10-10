6c2605c170d76356af0a40a934990160df518c12
dastigah <tony.pham@uconn.edu>
2018-10-09 23:55:19 -0400

    Initial commit with an addition of the parser
    Used this website as a tutorial: https://docs.python.org/2/howto/argparse.html

---
+++ main.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n == 0 : 
+        return 1
+    else :
+        return n * factorial(n-1)
+    
+
+#def stringReverse(n)
+    
+    
+if __name__ == "__main__":
+var = int(raw_input("Factorial Value: "))
+print factorial(var)
+var = raw_input("St")
