dfbbea2599dd5d29d996a77cc4b37bcb8ee0bd79
Tony <tony.pham@uconn.edu>
2018-10-11 08:52:42 +0000

    Added skeleton for classes and current supported calls

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import argparse
 
+import User			#TODO
+import FileStorage	#TODO
+import DBStorage	#TODO
+
+
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
     
@@ -8,11 +14,21 @@ if __name__ == "__main__":
 	exclusiveGroup.add_argument('-l','--list',action='store_true',help='List all users in directory')
 	exclusiveGroup.add_argument('-d','--delete',help='Delete user in directory')
     
-    parseer.add_argument('-f','--file',help='Use a local file instead of database')
+    parser.add_argument('-f','--file',help='Use a local file instead of database')
+	# Possibly need to include an option to choose DB connection string
     
 	parser.parse_args()
-    
-    if (
-    
-    
-    )
+
+	""" Decide on the storage type to use File vs Databse """
+	if parser.file:
+		userDirectory = FileStorage(parser.file)
+	else:
+		userDirectory = DBStorage(hostName,portNumber,isCredsReq)
+
+	""" Options """
+	if exclusiveGroup.create:
+		addUser(userDirectory,currentUser)		#TODO
+	elif exclusiveGroup.list:
+		listUsers(userDirectory)				#TODO
+	else:
+		deleteUser(userDirectory,currentUser)	#TODO
