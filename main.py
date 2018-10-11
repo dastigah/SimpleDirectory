c154b90aff44d61c75087841cfadeb16b96ff377
Tony Pham (desktop) <tony.pham@uconn.edu>
2018-10-10 23:48:16 -0400

    Added an option to add local files

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import argparse
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
+    
 	exclusiveGroup = parser.add_mutually_exclusive_group()
 	exclusiveGroup.add_argument('-c','--create',help='Create a new user in the directory')
 	exclusiveGroup.add_argument('-l','--list',action='store_true',help='List all users in directory')
 	exclusiveGroup.add_argument('-d','--delete',help='Delete user in directory')
+    
+    parseer.add_argument('-f','--file',help='Use a local file instead of database')
+    
 	parser.parse_args()
+    
+    if (
+    
+    
+    )
