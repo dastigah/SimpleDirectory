2f0a9e6d90e39b1cd3095bd2b5a5b05336fd4f08
Tony Pham <tony.pham@uconn.edu>
2018-10-13 16:06:29 -0400

    Added userAdd capabilities

--- UserDirectory.py
+++ UserDirectory.py
@@ -9,44 +9,42 @@ FORMAT:
     END
 """
 class FileStorage(object):
-    
-    def __init__(self,_fileName):
-        fileName = _fileName
+    def __init__(self, _fileName):
+            self.fileName = _fileName
 
-    
     def addUser(self, user):
-		try:
-			with open(fileName,'a+') as filePtr:
-				filePtr.write('START')
-				filePtr.write(user.toString())
-				filePtr.write('END')
-    	except IOError:
-			print 'Unable to add user to file'
+        try:
+            with open(self.fileName,'a+') as filePtr:
+                filePtr.write(user.toString())
+                filePtr.write('\n\n')
+        except IOError:
+            print 'Unable to add user to file'
 
     def deleteUser(self,user):
         pass #TODO
-    
-	""" Returns a list of all Users """
+
+    """ Returns a list of all Users """
     def getUsers(self,user):
-    	try:
-			with open(self.fileName,'r') as filePtr:
-				lines = filePtr.readlines()
-				for line in lines.split():
-					#write code to read in b/t start
-    
-    
-class DBStorage(object):
-    
+        try:
+            with open(self.fileName,'r') as filePtr:
+                lines = filePtr.readlines()
+                #for line in lines.split():
+                    #write code to read in b/t start
+        except IOError:
+            print 'Unable to get users'
+
+class DBStorage(object): 
+    pass
+
     def __init__(self,_host,_port):
-        fileName = _fileName
-    
+        self.host = _host
+        self.port = _port
+
     def addUser(self, user):
         pass #TODO
-    
+
     def deleteUser(self,user):
         pass #TODO
-    
+
     def listUsers(self,user):
         pass #TODO
-    
-    
